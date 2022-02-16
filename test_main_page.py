1c35a8afd17da843d6e9d303babf4ed7039b5558
Roma Tveritin <valinhairy@gmail.com>
2022-02-16 22:50:04 +0300

    add test for login page

--- test_main_page.py
+++ test_main_page.py
@@ -1,4 +1,5 @@
 from .page_objects.main_page import MainPage
+from .page_objects.login_page import LoginPage
 
 
 def test_guest_can_go_to_login_page(browser):
@@ -6,6 +7,8 @@ def test_guest_can_go_to_login_page(browser):
     page = MainPage(browser, link)
     page.open()
     page.go_to_login_page()
+    login_page = LoginPage(browser, browser.current_url)
+    login_page.should_be_login_page()
 
 
 def test_guest_should_see_login_link(browser):
